048240b19b01484151cd406c6e9568b2ac7a6a25
Combos93 <combos93@mail.ru>
2019-03-09 16:05:16 +0600

    decorate form of reset password

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -30,8 +30,6 @@ class PhotosController < ApplicationController
 
   private
 
-
-
   def notify_subscribers(event, photo)
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
     # all_emails = (event.photos.map(&:user_email)).uniq
