3d2ac86d60c664ea7158569a9ade5fabcce89a16
Combos93 <combos93@mail.ru>
2019-03-12 17:16:09 +0600

    secure event by pincode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_154256) do
+ActiveRecord::Schema.define(version: 2019_03_12_090820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,13 +27,14 @@ ActiveRecord::Schema.define(version: 2019_03_05_154256) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.text "title"
+    t.string "title"
     t.text "description"
-    t.text "address"
+    t.string "address"
     t.datetime "datetime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -59,19 +60,19 @@ ActiveRecord::Schema.define(version: 2019_03_05_154256) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "name"
-    t.text "email", default: "", null: false
+    t.string "name"
+    t.string "email", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "encrypted_password", default: "", null: false
-    t.text "reset_password_token"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.bigint "sign_in_count", default: 0, null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.text "current_sign_in_ip"
-    t.text "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -79,6 +80,7 @@ ActiveRecord::Schema.define(version: 2019_03_05_154256) do
 
   add_foreign_key "comments", "events"
   add_foreign_key "comments", "users"
+  add_foreign_key "events", "users"
   add_foreign_key "photos", "events"
   add_foreign_key "photos", "users"
   add_foreign_key "subscriptions", "events"
